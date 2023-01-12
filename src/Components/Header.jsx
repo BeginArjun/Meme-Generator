705f7c83c75ee3d280ebb25c72bb324c5487785a
BeginArjun <jayshrma161@gmail.com>
2023-01-12 19:12:25 +0530

    Applied Responsiveness

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 class Header extends React.Component{
     render(){
         return(
-            <div className='h-[64px] flex flex-row justify-around items-center w-screen p-[20px] bg-gradient-to-r from-[#672280] to-[#A626D3]'>
-                <div className='flex flex-row justify-between items-center gap-5 absolute left-4'>
-                    <img src='https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png'
-                    className='w-[31.31px] h-[26.22px]' alt='logo'></img>
-                    <p className='font-bold text-white text-xl leading-6 tracking-[-0.1em] font-Karla'>Meme Generator</p>
-                </div>
-                <p className='text-white font-semibold text-sm text-right leading-4 absolute right-6 font-Inter'>React Course - Project 3</p>
+            <div className='h-[64px] flex flex-row justify-start gap-5 items-center w-[100%] p-[20px] bg-gradient-to-r from-[#672280] to-[#A626D3]'>
+                <img src='https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png'
+                className='w-[31.31px] h-[26.22px]' alt='logo'></img>
+                <p className='font-bold text-white text-xl leading-6 tracking-[-0.1em] font-Karla'>Meme Generator</p>
             </div>
         )
     }
--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -45,14 +45,14 @@ export default function Meme(){
 
     return (
         <main>
-            <div className='flex flex-col justify-around items-center mt-3'>
-                <div className='flex flex-row justify-center items-center mb-5 font-sans'>
-                    <input type="text" className='px-3 w-[230px] h-[35px] rounded-md border border-[#D5D4D8] mr-2' name='topText' onChange={handleChange} value={meme.topText}/>
-                    <input type="text" className='px-3 w-[230px] h-[35px] rounded-md border border-[#D5D4D8] ml-2' name='bottomText' onChange={handleChange} value={meme.bottomText}/>
+            <div className='flex flex-col justify-around items-center mt-3 sm:justify-center'>
+                <div className='box-border flex flex-row justify-center items-center mb-5 font-sans sm:w-60 sm:flex-col sm:gap-3 md:w-64 md:flex-col md:gap-4'>
+                    <input type="text" className='px-3 w-[230px] h-[35px] rounded-md border border-[#D5D4D8]' name='topText' onChange={handleChange} value={meme.topText} placeholder='Top Text'/>
+                    <input type="text" className='px-3 w-[230px] h-[35px] rounded-md border border-[#D5D4D8]' name='bottomText' onChange={handleChange} value={meme.bottomText} placeholder='Bottom Text'/>
                 </div>
-                <button className='cursor-pointer bg-gradient-to-r from-[#672280] to-[#A626D3] w-[477px] h-[40px] rounded-md text-white font-Karla' onClick={randomMeme}>Get a new meme image 🖼</button>
+                <button className='cursor-pointer bg-gradient-to-r from-[#672280] to-[#A626D3] w-[477px] sm:w-96 h-[40px] sm:h-16 rounded-md text-white font-Karla' onClick={randomMeme}>Get a new meme image 🖼</button>
                 <div className='relative w-[477px] h-[350px] rounded-sm mt-5 drop-shadow-lg' style={style}>
-                    <h2 className="text-3xl font-['Impact'] drop-shadow-lg uppercase tracking-[1px] w-[80%] -translate-x-[50%] text-center absolute text-white left-1/2 top-4">{meme.topText}</h2>
+                    <h2 className="text-3xl font-['Impact'] drop-shadow-lg uppercase tracking-[1px] w-[80%] -translate-x-[50%] text-center absolute text-white left-1/2 top-5">{meme.topText}</h2>
                     <h2 className="text-3xl font-['Impact'] drop-shadow-lg uppercase tracking-[1px] w-[80%] -translate-x-[50%] text-center absolute text-white left-1/2 bottom-7">{meme.bottomText}</h2>
                 </div>
             </div>
